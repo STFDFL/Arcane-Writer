9681ab4cdfd7e78e957ed17ea57d89dd84d8ad06
jarrahholt <1002738@student.sae.edu.au>
2016-03-01 16:12:18 +1100

    Enemy Looking

---
+++ Assets/Scripts/FallingTorch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingTorch : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void OnTriggerEnter()
+	{
+		rb.GetComponent<Rigidbody>().useGravity = true;
+	}
+
+}
