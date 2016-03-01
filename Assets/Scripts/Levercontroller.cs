0b9786f97d2f764e9543f5bc72c558c64212f0e2
STFDFL <stedeflo@hotmail.com>
2016-03-01 16:00:25 +1100

    lever animation w gate

--- Assets/Scripts/Levercontroller.cs
+++ Assets/Scripts/Levercontroller.cs
@@ -10,12 +10,15 @@ public class Levercontroller : MonoBehaviour {
 
     public bool leverIsPulledLeft = false;
     public bool leverIsPulledRight = false;
-    public bool leverIsNeutral = true;
-    public new Animation animation;
+    
+    public Animation animationLever;
+   // public Animation animationGate;
+    public GameObject gate;
+    public bool gateIsOpen = false;
     // Use this for initialization
     void Start ()
     {
-        
+        //gate.GetComponent<Gate>();
     }
 	
 	// Update is called once per frame
@@ -23,27 +26,42 @@ public class Levercontroller : MonoBehaviour {
     {
         //animation.Play("right");
        // animation["On"].wrapMode = WrapMode.Once;
+       
+      
     }
 
     public void PullLeverLeft()
     {
         leverIsPulledRight = false;
-        leverIsNeutral = false;
         leverIsPulledLeft = true;
-        animation.Play("left");
+        animationLever.Play("left");
+        gateIsOpen = true;
+        if (gateIsOpen == true)
+        {
+            Debug.Log("im opening");
+            gate.GetComponent<Animation>().Play("openGate");
+        }
     }
     public void PullLeverRight()
     {
         leverIsPulledRight = true;
-        leverIsNeutral = false;
+
         leverIsPulledLeft = false;
-        animation.Play("right");
+        animationLever.Play("right");
+        gateIsOpen = false;
+        if (gateIsOpen == false)
+        {
+            gate.GetComponent<Animation>().Play("closeGate");
+            
+        }
     }
-    public void PullLeverNeutral()
-    {
-        leverIsPulledRight = false;
-        leverIsNeutral = true;
-        leverIsPulledLeft = false;
-        animation.Play("Default Take");
-    }
+    //public void PullLeverNeutral()
+    //{
+    //    leverIsPulledRight = false;
+    //    leverIsNeutral = true;
+    //    leverIsPulledLeft = false;
+    //    //animationLever.Play("Default Take");
+        
+       
+    //}
 }
