c618298d3802536f43d5cb9ad6d3acd013152ee1
STFDFL <stedeflo@hotmail.com>
2016-04-04 15:26:06 +1000

    loot system wip

---
+++ Assets/Scripts/LootThis.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootThis : MonoBehaviour
+{
+
+    private bool isLooted;
+    private int lootChance;
+    public AudioClip trapSound;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    public void Loot()
+    {
+        if(isLooted == false)
+        {
+            lootChance = Random.Range(0, 100);
+            if((lootChance >= 0) && (lootChance < 40)) // 40%
+            {
+                // nothing found
+                isLooted = true;
+            }
+            else if ((lootChance >= 40) && (lootChance < 70)) // 30%
+            {
+                //potion is found
+                AudioSource.PlayClipAtPoint(trapSound, transform.position);
+                isLooted = true;
+            }
+            else if ((lootChance >= 70) && (lootChance < 90)) // 20%
+            {
+                //trap is activated
+                
+                isLooted = true;
+            }
+            else if ((lootChance >= 90) && (lootChance <= 100)) // 10%
+            {
+                // coin is found
+
+                isLooted = true;
+            }
+            
+        }
+       
+    }
+}
