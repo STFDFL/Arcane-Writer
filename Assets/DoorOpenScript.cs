e38b6b754bf7b265ea2dac7aff0832e5225d4ec7
STFDFL <stedeflo@hotmail.com>
2016-04-11 13:52:21 +1000

    DoorAnim

---
+++ Assets/DoorOpenScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenScript : MonoBehaviour {
+
+    public bool DoorOpen;
+    private bool DoorOpened;
+
+	// Update is called once per frame
+	void Update () {
+        
+        if (DoorOpen == true && !DoorOpened){
+            gameObject.GetComponent<Animation>().Play("DoorOpenAnimation");
+            DoorOpened = true;
+        }
+    }
+}
