8c7b019c509bbc42e948b1b6a70de11bc689373e
STFDFL <stedeflo@hotmail.com>
2016-04-11 11:02:42 +1000

    end door tutorial

---
+++ Assets/Scripts/EndDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndDoor : MonoBehaviour {
+
+    GameObject triggerObject;
+    GasTrap gasTrap;
+    public GameObject openDoor;
+    public GameObject closedDoor;
+    // Use this for initialization
+    void Start ()
+    {
+        triggerObject = GameObject.Find("Gas Trap");
+        gasTrap = triggerObject.GetComponent<GasTrap>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if(gasTrap.trapEnabled == true)
+        {
+            closedDoor.SetActive(false);
+            openDoor.SetActive(true);
+
+        }
+	}
+}
