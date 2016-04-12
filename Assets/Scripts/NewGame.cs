69a86646e01001b78628d20d7d389f1f84ed4c04
jarrahholt <1002738@student.sae.edu.au>
2016-04-12 15:47:59 +1000

    Added Menu Placeholders
    - Main Menu
    - Death Screen
    - "Win" Screen

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+	public string nextLevel;
+
+	public void LoadLevel()
+	{
+		Application.LoadLevel(nextLevel);
+	}
+}
