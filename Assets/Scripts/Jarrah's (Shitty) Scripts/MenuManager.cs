baaf82fa318acf172e86213ca0fb3433ee128296
jarrahholt <1002738@student.sae.edu.au>
2016-04-14 12:48:12 +1000

    level 1 updates

---
+++ Assets/Scripts/Jarrah's (Shitty) Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	void Start()
+	{
+		Cursor.visible = true; 
+	}
+
+	public void LoadTutorial()
+	{
+		Application.LoadLevel(1);
+	}
+
+
+	public void MainMenu()
+	{
+		Application.LoadLevel (0);
+	}
+
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
